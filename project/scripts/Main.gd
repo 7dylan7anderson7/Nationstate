27a672a09776c65d3c5555e44565de14a7bdc176
Dylan Anderson <7dylan7anderson7@gmail.com>
2025-11-29 23:23:46 -0600

    User interface updates to prepare for economic model integration

--- project/scripts/Main.gd
+++ project/scripts/Main.gd
@@ -1,13 +1,13 @@
 extends Control
 
 
-@onready var quarter_label = $QuarterLabel
+@onready var quarter_label = $VBoxContainer/Panel/HBoxContainer/VBoxContainer/QuarterLabel
 
 func _ready():
 	update_label()
 
 func update_label():
-	quarter_label.text = "Year %s, Q%s" % [GameState.year, GameState.quarter]
+	quarter_label.text = " Year %s, Q%s" % [GameState.year, GameState.quarter]
 
 
 func _on_next_quarter_button_pressed() -> void:
